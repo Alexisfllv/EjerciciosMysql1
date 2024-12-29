45c18df0c268e28e07bb2409cf0bfb97d2e98331
Alexisfllv <alexis-fl-live@hotmail.com>
2024-12-29 18:42:03 -0500

    terminate 35

--- Mysql035.sql
+++ Mysql035.sql
@@ -144,5 +144,149 @@ select  ciudad,provincia ,count(*) as cantidad  from clientes
 group by ciudad ,provincia;
 
 
+-- Problema 2 
 
+drop table if exists visitas;
+
+create table visitas(
+numero int unsigned auto_increment,
+nombre varchar(30) not null,
+mail varchar(50),
+pais varchar(20),
+fecha date,
+primary key(numero)
+);
+
+select * from visitas;
+
+ insert into visitas (nombre,mail,fecha)
+  values ('Ana Maria Lopez','AnaMaria@hotmail.com','2006-10-10');
+ insert into visitas (nombre,mail,fecha)
+  values ('Gustavo Gonzalez','GustavoGGonzalez@hotmail.com','2006-10-10');
+ insert into visitas (nombre,mail,fecha)
+  values ('Juancito','JuanJosePerez@hotmail.com','2006-10-11');
+ insert into visitas (nombre,mail,fecha)
+  values ('Fabiola Martinez','MartinezFabiola@hotmail.com','2006-10-12');
+ insert into visitas (nombre,mail,fecha)
+  values ('Fabiola Martinez','MartinezFabiola@hotmail.com','2006-09-12');
+ insert into visitas (nombre,mail,fecha)
+  values ('Juancito','JuanJosePerez@hotmail.com','2006-09-12');
+ insert into visitas (nombre,mail,fecha)
+  values ('Juancito','JuanJosePerez@hotmail.com','2006-09-15');
+
+-- total de visitas
+select 
+count(*) as total_visitas
+from
+visitas; 
+
+-- cantidad de visitas agrupadas por fecha
+select fecha,
+count(*) as total
+from visitas
+group by fecha;
+
+
+
+
+-- cantidad de visitas agrupadas por nombre y mes
+select nombre,month(fecha),
+count(*) as cantidad
+from visitas
+group by nombre,month(fecha);
+
+
+-- Problema 3 
+drop table if exists empleados;
+
+create table empleados(
+documento char(8) not null,
+nombre varchar(30) not null,
+sexo char(1),
+domicilio varchar(30),
+fechaingreso date,
+fechanacimiento date,
+sueldobasico decimal(5,2) unsigned,
+primary key(documento)
+);
+
+describe empleados;
+
+insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('22333111','Juan Perez','m','Colon 123','1990-02-01','1970-05-10',550);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('25444444','Susana Morales','f','Avellaneda 345','1995-04-01','1975-11-06',650);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('20111222','Hector Pereyra','m','Caseros 987','1995-04-01','1965-03-25',510);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('30000222','Luis Luque','m','Urquiza 456','1980-09-01','1980-03-29',700);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('20555444','Maria Laura Torres','f','San Martin 1122','2000-05-15','1965-12-22',700);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('30000234','Alberto Soto','m','Peru 232','2003-08-15','1989-10-10',420);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('20125478','Ana Gomez','f','Sarmiento 975','2004-06-14','1976-09-21',350);
+ insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('24154269','Ofelia Garcia','f','Triunvirato 628','2004-09-23','1974-05-12',390);
+insert into empleados (documento,nombre,sexo,domicilio,fechaingreso,fechanacimiento,sueldoBasico)
+  values ('30154269','Oscar Mendez','m','Colon 1245','2004-06-23','1984-05-14',300);
+
+-- contar cantindad de masculino y femenino de empleados
+select 
+sexo , count(sexo)
+from empleados
+where month(fechanacimiento)=5
+group by sexo;
+
+
+-- conocer la cantidad de empleados agrupados por anio de ingreso a la empresa
+select 
+year(fechaingreso),
+count(*) as empleados
+from empleados
+group by fechaingreso;
+
+-- Problema 4 Facturas
+drop table if exists facturas;
+
+create table facturas(
+numero int(10) zerofill,
+descripcion varchar(30),
+precioporunidad decimal(5,2) unsigned,
+cantidad tinyint unsigned
+);
+
+describe facturas;
+
+
+ insert into facturas values(504,'escuadra 20 cm.',2.5,100);
+ insert into facturas values(504,'escuadra 50 cm.',5,80);
+ insert into facturas values(2002,'compas plastico',8,120);
+ insert into facturas values(2002,'compas metal',15.4,100);
+ insert into facturas values(2002,'escuadra 20 cm.',2.5,100);
+ insert into facturas values(4567,'escuadra 50 cm.',5,200);
+
+select * from facturas;
+-- cuenta la cantidad de items por factura
+select
+numero as cantindadItems, 
+count(*)
+from facturas
+group by numero;
+
+
+-- sumar la cantidad de productos por facturas
+select 
+numero,
+sum(cantidad) as cantidadProductos
+from facturas
+group by numero;
+
+-- total de dinero de facturas
+
+select
+numero,
+sum(precioporunidad * cantidad) as totalDinero
+from facturas
+group by numero;
 
