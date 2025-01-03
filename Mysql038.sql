11897bfcc38ffd468eb44801ca1c8e51081ca5f4
Alexisfllv <alexis-fl-live@hotmail.com>
2025-01-03 12:22:52 -0500

    terminate 38 Alias

--- Mysql038.sql
+++ Mysql038.sql
@@ -69,9 +69,51 @@ drop table if exists clientes;
   primary key(codigo)
  );
 
+insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Lopez Marcos', 'Colon 111', 'Córdoba','Cordoba','null');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Perez Ana', 'San Martin 222', 'Cruz del Eje','Cordoba','4578585');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Garcia Juan', 'Rivadavia 333', 'Villa Maria','Cordoba','4578445');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Perez Luis', 'Sarmiento 444', 'Rosario','Santa Fe',null);
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono) 
+  values ('Pereyra Lucas', 'San Martin 555', 'Cruz del Eje','Cordoba','4253685');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Gomez Ines', 'San Martin 666', 'Santa Fe','Santa Fe','0345252525');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Torres Fabiola', 'Alem 777', 'Villa del Rosario','Cordoba','4554455');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Lopez Carlos', 'Irigoyen 888', 'Cruz del Eje','Cordoba',null);
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Ramos Betina', 'San Martin 999', 'Cordoba','Cordoba','4223366');
+ insert into clientes (nombre,domicilio,ciudad,provincia,telefono)
+  values ('Lopez Lucas', 'San Martin 1010', 'Posadas','Misiones','0457858745');
+select * from clientes;
 
+-- obtener el total de registros que no tienen valor nulo en los telefono , uso de alias
 
+select
+count(*) as registros
+from clientes
+where telefono is not null;
 
+select 
+count(telefono) as 'con telefono'
+from clientes;
+
+-- muestre cantidad de clientes que se apellidan Perez , colocando un alias para dicha salida;
+
+select 
+count(nombre) as "llevan Perez"
+from clientes
+where nombre like "%Perez%";
+
+-- obtener la cantidad de ciudades distintas por provincia las cuales hay cleintes , coloque alias
+
+select
+count(distinct ciudad) as "cantidades de ciudades disintas"
+from clientes;
 
 
 
