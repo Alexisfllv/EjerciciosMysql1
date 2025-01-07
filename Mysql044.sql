e2ac8e933d7a0e99095e00f8ff63a1897ed7e024
Alexisfllv <alexis-fl-live@hotmail.com>
2025-01-07 16:23:30 -0500

    drop index 44

--- Mysql044.sql
+++ Mysql044.sql
@@ -32,14 +32,51 @@ drop table if exists alumnos;
 
 
 create table alumnos(
-  año year not null,
+  anio year not null,
   numero int unsigned not null,
   nombre varchar(30),
   documento char(8) not null,
   domicilio varchar(30),
   ciudad varchar(20),
   provincia varchar(20),  
-  primary key(año,numero),
+  primary key(anio,numero),
   unique i_documento (documento),
-  index i_ciudadprovincia (ciudad,provincia),
+  index i_ciudadprovincia (ciudad,provincia)
+);
+ 
+describe alumnos;
+
+show index from alumnos;
+ 
+ 
+ -- problema 2 consultas
+ 
+drop table if exists consultas;
+
+
+create table consultas(
+  fecha date,
+  numero int unsigned,
+  documento char(8) not null,
+  obrasocial varchar(30),
+  medico varchar(30),
+  primary key(fecha,numero),
+  unique i_consulta(documento,fecha,medico),
+  index i_medico (medico),
+  index i_obrasocial (obrasocial)
  );
+
+show index from consultas;
+ 
+ 
+drop index i_consulta on consultas;
+
+drop index i_medico on consultas;
+ 
+drop index i_obrasocial on consultas;
+
+describe consultas;
+
+ 
+ 
+ 
