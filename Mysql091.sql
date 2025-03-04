4df8dd90a82607b18fee2246cff3e32510d8119f
Alexisfllv <alexis-fl-live@hotmail.com>
2025-03-04 10:13:27 -0500

    Subconsulta simil autocombinación 91

--- Mysql091.sql
+++ Mysql091.sql
@@ -113,3 +113,88 @@ join libros as l2
 on l1.editorial = l2.editorial
 group by l1.editorial , l1.titulo, l1.precio
 having l1.precio > avg(l2.precio);
+
+
+-- prob 1 deportes
+
+drop table if exists deportes;
+
+
+
+create table deportes(
+nombre varchar(15),
+profesor varchar(30),
+dia varchar(10),
+cuota decimal(5,2)
+);
+
+
+insert into deportes values('tenis','Ana Lopez','lunes',20);
+insert into deportes values('natacion','Ana Lopez','martes',15);
+insert into deportes values('futbol','Carlos Fuentes','miercoles',10);
+insert into deportes values('basquet','Gaston Garcia','jueves',15);
+insert into deportes values('padle','Juan Huerta','lunes',15);
+insert into deportes values('handball','Juan Huerta','martes',10);
+
+
+select * from deportes;
+
+-- nombres de profesores que dictan mas de un deporte empleando subconsulta
+select distinct d1.profesor
+from deportes as d1
+where d1.profesor in
+(select d2.profesor
+from deportes as d2
+where d1.nombre <> d2.nombre);
+
+
+-- mejor
+
+SELECT profesor
+FROM deportes
+GROUP BY profesor
+HAVING COUNT(DISTINCT nombre) > 1;
+
+
+--
+select distinct d1.profesor
+from deportes as d1
+join deportes as d2
+on d1.profesor=d2.profesor
+where d1.nombre<>d2.nombre;    
+
+
+-- deportes que dictan el mismo dia que un determinado deporte
+
+select nombre
+from deportes
+where nombre <> 'natacion' and
+dia = 
+(select dia from deportes
+where nombre = 'natacion');
+
+
+-- join
+
+select d1.nombre
+from deportes as d1
+join deportes as d2
+on d1.dia = d2.dia
+where d2.nombre = 'natacion' and
+d1.nombre <> d2.nombre;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
