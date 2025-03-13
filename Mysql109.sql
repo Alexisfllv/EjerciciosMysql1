63a34ffb97bc18a15727d842e50cb5c291ef7644
Alexisfllv <alexis-fl-live@hotmail.com>
2025-03-12 20:52:20 -0500

    funciones almacenadas 109

--- Mysql109.sql
+++ Mysql109.sql
@@ -55,3 +55,71 @@ delimiter ;
 
 select url,f_estrellas(estrellas) from sitios;
 
+-- prob 2 , traficos
+
+drop function if exists f_tipositio;
+
+delimiter //
+create function f_tipositio(
+	cantidad int
+)
+	returns varchar(20)
+	deterministic
+begin
+	case
+		when cantidad < 20000000 then
+			return 'trafico bajo';
+        when cantidad >= 20000000 and cantidad < 40000000 then
+			return 'trafico medio';
+		when cantidad >= 40000000 then
+			return 'trafico alto';
+	end case;
+end //
+delimiter ;
+
+select url,f_estrellas(estrellas), cantpaginas, f_tipositio(cantpaginas) from sitios;
+
+-- mayor trafico
+
+drop function if exists f_mayor_trafico;
+
+delimiter //
+create function f_mayor_trafico()
+	returns varchar(100)
+    deterministic
+begin
+	declare vurl varchar(100);
+    select url into vurl 
+    from sitios 
+    order by cantpaginas desc limit 1;
+    return vurl;
+end //
+delimiter ;
+
+select f_mayor_trafico();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
